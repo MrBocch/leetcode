1923714fc376510bd01250e35c0053dd6b0b06a6
MrBochh <jorgealberto1436@gmail.com>
2023-08-17 15:47:17 -0600

    .

--- 1672_RichestCustomerWealth/1672.c
+++ 1672_RichestCustomerWealth/1672.c
@@ -1,11 +1,19 @@
+#include <stdio.h>
+
 int maximumWealth(int** accounts, int accountsSize, int* accountsColSize){
-    int maxwealth = 0;
+    
+    int maxWealth = 0;
+    int currentWealth = 0;
+
     for(int i = 0; i < accountsSize; i++){
+        
     int currentWealth = 0;
         for(int j = 0; j < accountsColSize[0]; j++){
+            
             currentWealth += accounts[i][j];
         }
-        if(maxwealth < currentWealth) maxwealth = currentWealth;
+        //if(maxWealth < currentWealth) maxWealth = currentWealth;
+        maxWealth = fmax(maxWealth, currentWealth);
     }   
-    return maxwealth;
+    return maxWealth;
 }
