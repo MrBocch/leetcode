f1aabca18735d78d987d8f56904390e5cdb2bb44
MrBochh <jorgealberto1436@gmail.com>
2023-09-30 17:48:59 -0600

    quickly scrapped junk code but works

--- helper.py
+++ helper.py
@@ -4,11 +4,10 @@ import pyperclip as pc
 # to paste from clipboard 
 # pyperclip.paste()
 
-
+# not needed anymore 
 # def makeNotes():
 #     dirlist = os.listdir(".")
 #     today = str(date.today()) + "\n"
-
 #     for d in dirlist:
 #         if os.path.isdir(d) and not d==".git":
 #             # print(f"{d} is a folder")
@@ -52,6 +51,58 @@ def solvedNew():
         print("Do you have other solutions? (y/n) ")
         nsol = input()
 
+# what if already solved but with a different language?
+def oldNewLang():
+    n = input("What problem number: ")
+    dirlist = os.listdir(".")
+    folder = ""
+    fnum = ""
 
+    for d in dirlist:
+        if os.path.isdir(d) and not d ==".git":
+            fsplit = d.split("_")
+            fnum = fsplit[0]
+            fname = fsplit[1]
+            if fnum == n:
+                folder = f"{fnum}_{fname}"
+                break
 
-solvedNew()
+    if folder == "":
+        print("Problem not found")
+        return
+
+    print("Make sure to have solution in clipboard b4 type extension")
+    ext = input("extension: .")
+    sol = pc.paste()
+    sol = sol.replace('\r\n', '\r')
+
+    with open(f"{folder}/{fnum}.{ext}", "w") as f:
+        f.write(sol)
+ 
+    print("Do you have other solutions? (y/n) ")
+    nsol = input()
+    while nsol == "y":
+        print("Make sure to have solution in clipboard b4 type extension")
+        ext = input("extension: .")
+        sol = pc.paste()       
+        sol = sol.replace('\r\n', '\r')
+        with open(f"{folder}/{fnum}.{ext}", "w") as f:
+            f.write(sol)
+
+        print("Do you have other solutions? (y/n) ")
+        nsol = input()
+
+exit = False
+while not exit:
+    print("(1). Solved a new problem")
+    print("(2). Solved a old problem in diff new lang")
+    print("(3). Exit")
+    op = int(input("> "))
+    if op == 3:
+        exit = True
+    
+    if op == 1:
+        solvedNew()
+
+    if op == 2:
+        oldNewLang()
