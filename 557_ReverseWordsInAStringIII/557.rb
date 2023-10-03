d05d350c9d34ce791402b199c90e4057cdd290f5
MrBochh <jorgealberto1436@gmail.com>
2023-10-03 13:41:40 -0600

    fix?

--- 557_ReverseWordsInAStringIII/557.rb
+++ 557_ReverseWordsInAStringIII/557.rb
@@ -1,1 +1,5 @@
-# @param {String} s# @return {String}def reverse_words(s)   return s.split(" ").each{|w| w.reverse!}.join(" ")end
+# @param {String} s
+# @return {String}
+def reverse_words(s)
+   return s.split(" ").each{|w| w.reverse!}.join(" ")
+end
