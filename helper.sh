29be702e8cf2cdf2a74f7d63ee2e209615b33260
MrBocch <jorgealberto1436@gmail.com>
2024-07-12 11:30:44 -0600

    so much fun

--- helper.sh
+++ helper.sh
@@ -7,30 +7,65 @@ PLANG=
 _get_name(){
     echo "insert number"
     read PNUM
+    echo
+
     echo "insert NameOfProblem"
     read PNAME
-    echo "insert extension, ex: .py"
+    echo
+
+    echo "insert extension, ex: [py | rb | hs | rs]"
     echo "make sure to have code in clipboard"
     read PLANG
+    echo
 }
 
 _paste(){
     if (uname="Darwin") then
         pbpaste > $FILE
     else
-        # xclip -selection clipboard -o > to file
-        echo "linux"
+        xclip -selection clipboard -o > $FILE
+        # echo "linux"
     fi
 }
 
 _main(){
+    while TRUE; do
+        echo "(1) Solved a new problem"
+        echo "(2) Solved a old problem in different lang"
+        echo "(3) Push to github"
+        echo "(4) Exit"
+        read c
+
+        case $c in
+            1)
+                ;;
+            2)
+                ;;
+            3)
+                git add .
+                git commit
+                git push
+                ;;
+            4)
+                exit
+                ;;
+            *)
+                echo "what?"
+                ;;
+        esac
+    done
     _get_name
     FOLDER="${PNUM}_${PNAME}"
     FILE="${PNUM}.${PLANG}"
 
     mkdir $FOLDER
     (cd $FOLDER && _paste)
-    (date -I > "$FOLDER/notes.txt")
+    date -I > "$FOLDER/notes.txt"
 }
 
+echo "
+====================
+= LEETCODE MANAGER =
+====================
+"
 _main
