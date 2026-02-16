fc4d109691885689cfb131a55e1ca3a55d841874
MrBocch <jorgealberto1436@gmail.com>
2026-02-15 23:09:46 -0600

    120

---
+++ src/120_Triangle/120.go
@@ -0,0 +1,14 @@
+func min(a int, b int) int {
+    if a < b { return a }
+    return b 
+}
+
+func minimumTotal(triangle [][]int) int {
+    for i := len(triangle) - 2 ; i >= 0 ; i -- {
+        for j := 0; j < len(triangle[i]); j++ {
+            triangle[i][j] += min(triangle[i+1][j], triangle[i+1][j+1])
+        } 
+    }
+
+    return triangle[0][0] 
+}
